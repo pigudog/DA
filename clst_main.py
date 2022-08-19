fd2389ebb0ebe46b4cefb0bd591b42c262275e29
pigudogzyy <21301050051@m.fudan.edu.cn>
2022-08-19 23:04:08 +0800

    pc3080

--- clst_main.py
+++ clst_main.py
@@ -2,7 +2,7 @@ from turtle import circle
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_circles,make_blobs,make_moons
-
+from sklearn.cluster import KMeans,DBSCAN,AgglomerativeClustering
 
 n_samples = 1000
 circles = make_circles(n_samples=n_samples,factor=0.5,noise=0.05)
@@ -12,9 +12,12 @@ blobs = make_blobs(n_samples=n_samples,random_state=8)
 random_data = np.random.rand(n_samples,2),None
 
 colors="bgrcmyk"
-data = [circles,moons,random_data]
+data = [circles,moons,blobs,random_data]
 
-models = [("None",None)]
+models = [("None",None),("KMeans",KMeans(n_clusters=3)),
+          ("DBSCAN",DBSCAN(min_samples=3,eps=0.5)),
+          ("Agglomerative",AgglomerativeClustering(n_clusters=3,linkage="ward"))]
+from sklearn.metrics import silhouette_score
 f = plt.figure()
 for inx,clt in enumerate(models):
     clt_name,clt_entity = clt
@@ -22,9 +25,16 @@ for inx,clt in enumerate(models):
         X,Y = dataset
         if not clt_entity:
             clt_res = [0 for item in range(len(X))]
+        else:
+            clt_entity.fit(X)
+            clt_res = clt_entity.labels_.astype(int)
         f.add_subplot(len(models),len(data),inx*len(data)+i+1)
-        [plt.scatter(X[p,0],X[p,1],color=colors[clt_res
-                                                    [p]]) for p in range(len(X))]
-    plt.show()
+        try:
+            print(clt_name,i,silhouette_score(X,clt_res))
+        except:
+            pass
+        [plt.scatter(X[p,0],X[p,1],color=colors[clt_res[p]]) for p in range(len(X))]
+        
+plt.show()
             
             
--- modeling.py
+++ modeling.py
@@ -91,9 +91,34 @@ def hr_modeling(features,label):
     from sklearn.linear_model import LogisticRegression
     from sklearn.ensemble import GradientBoostingClassifier
     
-    # from keras.models import Sequential
-    # from keras.layers.core import Dense,Activation
-    # from keras.optimizers import SGD
+    from keras.models import Sequential
+    from keras.layers.core import Dense,Activation
+    from keras.optimizers import SGD
+    
+    # mdl = Sequential()
+    # mdl.add(Dense(50,input_dim=len(f_v[0])))
+    # mdl.add(Activation("sigmoid"))
+    # mdl.add(Dense(2))
+    # mdl.add(Activation("softmax"))
+    # sgd = SGD(lr=0.01)
+    # mdl.compile(loss="mean_squared_error",optimizer="adam")
+    # mdl.fit(X_train,np.array([[0,1] if i==1 else [1,0] for i in Y_train]),epochs=10,batch_size=2048)
+    # xy_lst = [(X_train,Y_train),(X_validation,Y_validation),(X_test,Y_test)]
+    # import matplotlib.pyplot as plt
+    # from sklearn.metrics import roc_curve,auc,roc_auc_score
+    # f = plt.figure()
+    # for i in range(len(xy_lst)):
+    #     X_part = xy_lst[i][0]
+    #     Y_part = xy_lst[i][1]
+    #     Y_pred = mdl.predict(X_part)
+    #     print(Y_pred)
+    #     Y_pred = np.array(Y_pred[:,1]).reshape((1,-1))[0]
+    #     f.add_subplot(1,3,i+1)
+    #     fpr,tpr,threshold = roc_curve(Y_part,Y_pred)
+    #     plt.plot(fpr,tpr)
+    #     print("NN","AUC",auc(fpr,tpr))
+    #     print("NN","AUC_Score",roc_auc_score(Y_part,Y_pred))
+    # plt.show()
     
     ## Artificial Neural Network
     # mdl = Sequential()
@@ -122,11 +147,10 @@ def hr_modeling(features,label):
     # models.append(("BernoulliNB",BernoulliNB()))
     # models.append(("DecisionTree",DecisionTreeClassifier()))
     # models.append(("SVM Classifier",SVC()))
-    # models.append(("SVM Classifier",SVC()))
-    # models.append(("RandomForest",DecisionTreeClassifier()))
+    # models.append(("RandomForest",RandomForestClassifier)))
     # models.append(("Adaboost",AdaBoostClassifier()))
     # models.append(("LogisticRegression",LogisticRegression()))
-    models.append(("GBDT",GradientBoostingClassifier(max_depth=6,n_estimators=100)))
+    # models.append(("GBDT",GradientBoostingClassifier(max_depth=6,n_estimators=100)))
 
     list3 = ["test:","validation:","test:"]
     for clf_name,clf in models:
